854fa4bdfe93635d9681df1f602283762d139d35
fatima mabrouk <onlyfefe03@gmail.com>
2020-05-03 07:04:04 +0100

    folder-c npm validation packges

--- node-app/notes-app/app.js
+++ node-app/notes-app/app.js
@@ -1,4 +1,9 @@
+const validator = require('validator');
 const add = require('./utlis');
 
 const result = add(2,8);
 console.log(result);
+
+console.log(validator.isEmail('onlyfef@gmail.com'));
+// check the url ...
+console.log(validator.isURL('http://google.com'))
