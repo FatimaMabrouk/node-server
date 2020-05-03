411f3a4bf67c03dd9537fa6dc32f88b87957b72b
fatima mabrouk <onlyfefe03@gmail.com>
2020-05-03 06:00:04 +0100

    folder-3 notes-app

--- node-app/app.js
+++ node-app/app.js
@@ -1,1 +1,6 @@
-console.log('testng');
+const fs = require('fs');
+
+fs.writeFileSync('text.txt', 'first node app ...');
+
+// challenge - answer
+fs.appendFileSync('text.txt', ' adding new information in the file using appenf .');
