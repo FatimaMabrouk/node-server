09a3901611d9de37791ba793485679f68cc9f05e
fatima mabrouk <onlyfefe03@gmail.com>
2020-05-03 06:57:38 +0100

    folder/3 npm Packages

--- node-app/app.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-fs.writeFileSync('text.txt', 'first node app ...');
-
-// challenge - answer
-fs.appendFileSync('text.txt', ' adding new information in the file using appenf .');
-// with new file .
-fs.appendFileSync('notes.txt', 'adding new info to new file ...')
